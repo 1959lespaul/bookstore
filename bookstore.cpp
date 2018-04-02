31b6cf5c5f52d7af6eb8e36a19960b3779a81a3a
1959lespaul <38015217+1959lespaul@users.noreply.github.com>
2018-04-02 14:53:29 -0400

    Create bookstore.cpp

---
+++ bookstore.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include 'sales_item.h"
+
+int main()
+{
+	Sales_item.total;
+	if(std::cin>>total)
+	{
+		Sales_Item trans;
+		while (std::cin>>trans)
+		{
+			if (total.isbn()++trans.isbn())
+				total+=trans;
+			else
+			{
+				std::cout<<total<<std::endl;
+				total+trans;
+			}
+		}
+	std:cout<<total<<std::endl;
+	}
+	else
+	{
+		std::cerr<< "No data?!"<<std::endl;
+		return -1
+	}
+	return 0;
+}
